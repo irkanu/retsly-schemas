fc29e13ec906730c5ec6a6e6cfed72ae3bf67cf3
matsam <MadManMathew@gmail.com>
2015-05-13 12:56:10 -0700

    - remove old schema ref in openhouse

--- openhouse.js
+++ openhouse.js
@@ -1,16 +1,13 @@
 
-var RestlySchema = require('./schema');
-
-var openhouseSchema = {
-  openhouseID: {type:'string', project:'OpenHouseKey'},
-  listingID: {type:'string', project:'ListingKey'},
-  date: {type:'string', project:'OpenHouseDate'},
-  startTime: {type:'string', project:'OpenHouseStartTime'},
-  endTime: {type:'string', project:'OpenHouseEndTime'},
-  agentID: {type:'string', project:'ShowingAgentKey'},
-  type: {type:'string', project:'OpenHouseType'},
-  remarks: {type:'string', project:'OpenHouseRemarks'},
-  status: {type:'string', project:'Status'}
+module.exports = {
+  openhouseID: {type:'string'},
+  listingID: {type:'string'},
+  date: {type:'string'},
+  startTime: {type:'string'},
+  endTime: {type:'string'},
+  agentID: {type:'string'},
+  type: {type:'string'},
+  remarks: {type:'string'},
+  status: {type:'string'},
+  _listing: {type:'string'}
 };
-
-module.exports = new RestlySchema(openhouseSchema);
