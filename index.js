87547052135d05e193c49faa7b6d125931cca33d
matsam <MadManMathew@gmail.com>
2015-05-19 16:34:11 -0700

    -change ingo npm module

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-var agent = require('./agent');
-var office = require('./office');
-var listing = require('./listing');
-var openhouse = require('./openhouse');
-var media = require('./media');
+var agent = require('./lib/agent');
+var office = require('./lib/office');
+var listing = require('./lib/listing');
+var openhouse = require('./lib/openhouse');
+var media = require('./lib/media');
 
 module.exports = {
   agent: agent,
